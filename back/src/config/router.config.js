3453aa49a90e688767a15230fbaf2cf99394ee6e
StephAc <stephane.achari@gmail.com>
2020-06-22 15:19:47 +0000

    Revert "Merge branch 'api/feature/crud_user' into 'develop'"
    This reverts merge request !3

--- back/src/config/router.config.js
+++ back/src/config/router.config.js
@@ -1,10 +1,6 @@
 import { Router } from 'express';
 
-import UserController from '../controllers/User.controller';
-import {
-  checkIfCredentialsExist,
-  encryptPassword,
-} from '../middleware/credentials.middleware';
+import { checkIfCredentialsExist, encryptPassword } from '../middleware/credentials.middleware';
 import { registerUser, loginUser } from '../controllers/Auth.controller';
 
 const router = Router();
@@ -14,17 +10,13 @@ const URL_CONSTANT = {
 };
 
 // Auth
-router.post(
-  `/${URL_CONSTANT.USER}/register`,
-  [checkIfCredentialsExist, encryptPassword],
-  registerUser,
-);
+router.post(`/${URL_CONSTANT.USER}/register`, [checkIfCredentialsExist, encryptPassword], registerUser);
 router.post(`/${URL_CONSTANT.USER}/login`, loginUser);
 
 // User
-router.get(`/${URL_CONSTANT.USER}`, UserController.get);
-router.get(`/${URL_CONSTANT.USER}/:id`, UserController.getById);
-router.put(`/${URL_CONSTANT.USER}/:id`, UserController.updateById);
-router.delete(`/${URL_CONSTANT.USER}/:id`, UserController.deleteById);
+router.get(`/${URL_CONSTANT.USER}`);
+router.post(`/${URL_CONSTANT.USER}`);
+router.put(`/${URL_CONSTANT.USER}`);
+router.delete(`/${URL_CONSTANT.USER}`);
 
 export default router;
