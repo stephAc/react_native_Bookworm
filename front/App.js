3bba2aeb5fe8c26b4816d6c1cabdd0159099b16f
Julien Queant <aimrixxfr@gmail.com>
2020-07-05 16:37:56 +0200

    fix(app): Remove duplicate module

--- front/App.js
+++ front/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 import { createAppContainer } from 'react-navigation';
 import {
   createStackNavigator,
